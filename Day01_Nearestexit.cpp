44b08b3433e9ee9025a76accdf64cc56721ba4e5
Vidya <1980vidyam@gmail.com>
2025-09-30 05:45:15 +0530

    Add files via upload

---
+++ Day01_Nearestexit.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int T,X;
+	cin>>T;
+	for(int i=0;i<T;i++){
+	    cin>>X;
+	    if(X<=50){
+	        cout<<"LEFT"<<endl;;
+	        
+	    }
+	    else{
+	        cout<<"RIGHT"<<endl;
+	    }
+	}
+	return 0;
+
+}
