b982dfef2169a82fd736319e3e163d7baf7d729c
Vidya <1980vidyam@gmail.com>
2025-10-01 17:03:31 +0530

    Add files via upload

---
+++ Day04_GoodProgram.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int T;
+	cin>>T;
+	for(int i=0;i<T;i++){
+	    int N;
+	    cin>>N;
+	    if(N%4==0){
+	        cout<<"Good"<<endl;
+	    }
+	    else{
+	        cout<<"Not Good"<<endl;
+	    }
+	}
+	return 0;
+
+}
