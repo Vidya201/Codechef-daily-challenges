e76838b8bf4fde4aeb325034a09cd834b5813dd7
Vidya <1980vidyam@gmail.com>
2025-10-01 17:04:06 +0530

    Add files via upload

---
+++ Day05_conditionset.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int T;
+	cin>>T;
+	for(int i=0;i<T;i++){
+	    int A,B,C;
+	    cin>>A>>B>>C;
+	    if(A<=B && C<=B){
+	        cout<<"Yes"<<endl;
+	    }
+	    else{
+	        cout<<"No"<<endl;
+	        
+	    }
+	    
+	    
+	    
+	}
+	return 0;
+
+}
